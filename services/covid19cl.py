2f45b625c3d675138b6b0699ea4e3219540fa958
Jonathan Gutierrez <contacto@jgutierrez.cl>
2020-05-29 14:08:22 -0400

    adapt covid module to new structure

--- services/covid19cl.py
+++ services/covid19cl.py
@@ -1,7 +1,7 @@
 import json
 
 import requests
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from lib.common import find_str
 
@@ -9,17 +9,17 @@ view = Blueprint('covid19cl', __name__)
 
 step1 = 'https://e.infogram.com/d9e30e4b-e63c-4e02-a72a-eca4653f3283'
 datamap = {
-    'asintomaticos': 0,
-    'conectados': 1,
-    'activos': 4,
-    'confirmados': 7,
-    'sintomaticos': 11,
-    'total_examenes': 16,
-    'recuperados': 21,
-    'criticos': 26,
-    'examenes': 28,
-    'fallecidos': 5,
-    'fecha': 23
+    'asintomaticos': 21,
+    'conectados': 17,
+    'activos': 28,
+    'confirmados': 10,
+    'sintomaticos': 27,
+    'total_examenes': 1,
+    'recuperados': 4,
+    'criticos': 7,
+    'examenes': 32,
+    'fallecidos': 6,
+    'fecha': 9
 }
 
 
@@ -34,16 +34,19 @@ def show():
     data = data_raw.copy()
     data = data['elements']['content']['content']['entities']
     data = [v for k, v in data.items() if v['type'] in ['TEXT', 'SHAPE']]
-    data = [v['props']['content']['blocks'][0] for v in data
+    data = [v['props']['content']['blocks'][0]['text'] for v in data
             if 'content' in v['props'] and len(v['props']['content']['blocks']) > 0]
 
     data_result = {}
     for k, v in datamap.items():
-        data_result[k] = data[v]['text']
+        data_result[k] = data[v]
         if k != 'fecha':
             try:
                 data_result[k] = int(data_result[k].replace('.', ''))
             except ValueError:
                 data_result[k] = None
+
+    if request.args.get('raw', '') == '1':
+        data_result['raw'] = data
 
     return jsonify(data_result)
